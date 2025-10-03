068cf63c230f9fc9a90cadd0a0bf836189772c53
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-03 19:13:06 -0300

    make for

--- slices-and-arrays/02-make-and-for.go
+++ slices-and-arrays/02-make-and-for.go
@@ -3,6 +3,7 @@ package slicesandarrays
 import "fmt"
 
 func MakeAndFor() {
+
 	lista := []int{4, 9, 7}
 	lista = append(lista, 19)
 
@@ -10,12 +11,11 @@ func MakeAndFor() {
 
 	listaDeString = append(listaDeString, "Solidity")
 
-	fmt.Println("Lista de String: ", lista)
+	fmt.Println("Lista de String: ", lista, listaDeString)
 
-	fmt.Println("Lista: ", lista)
+	fmt.Println("Lista: ", lista, listaDeString)
 	fmt.Println("Lista: ", lista[0])
 	fmt.Println("Lista: ", lista[1])
 	fmt.Println("Lista: ", lista[2])
 	fmt.Println("Tamanho da lista: ", len(lista))
-
 }
