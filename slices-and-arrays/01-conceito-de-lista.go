4248e04477f98e04951902b013127e85562e02dd
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-03 17:35:28 -0300

    Criando lista e reorganizando packeges

---
+++ slices-and-arrays/01-conceito-de-lista.go
@@ -0,0 +1,13 @@
+package slicesandarrays
+
+import "fmt"
+
+func ConceitoDeLista() {
+
+	lista := []int{4, 9, 7}
+	fmt.Println("Lista: ", lista)
+	fmt.Println("Lista: ", lista[0])
+	fmt.Println("Lista: ", lista[1])
+	fmt.Println("Lista: ", lista[2])
+	fmt.Println("Tamanho da lista: ", len(lista))
+}
