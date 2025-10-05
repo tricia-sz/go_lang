f879792a66ec5a1252f79a34479f3ba26a2740c8
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-05 12:19:27 -0300

    Retorno

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	resultado := Soma
-	fmt.Println(resultado)
+	soma, subtracao, divisao, multiplicacao := Operacao(1, 2)
+	fmt.Println(soma, subtracao, divisao, multiplicacao)
 }
 
-func Soma(numero1 int, numero2 int) int {
-	resultado := numero1 + numero2
-	return resultado
+func Operacao(numero1 int, numero2 int) (int, int, int, int) {
+	soma := numero1 + numero2
+	subtracao := numero1 - numero2
+	divisao := numero1 / numero2
+	multiplicacao := numero1 * numero2
+
+	return soma, subtracao, divisao, multiplicacao
 }
