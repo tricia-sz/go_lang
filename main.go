974752ad44c12596fb28aca3e4f3bec84b781e3b
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-12 13:54:57 -0300

    Generics

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
-import (
-	"fmt"
-	model "learngo/model"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Iniciando...")
-	automovelMoto := model.Automovel{
-		Ano:    2022,
-		Placa:  "TRX-2020",
-		Modelo: "BMW",
+	slice1 := []int{5, 1, 2, 3}
+	slice2 := []string{"A", "B", "C", "D"}
+	newInts := reverse((slice1))
+	newString := reverse((slice2))
+	fmt.Println(newInts)
+	fmt.Println(newString)
+}
+
+func reverse[T int | string](slice []T) []T {
+	newInts := make([]T, len(slice))
+
+	newItensLen := len(slice) - 1
+	for i := 0; i < len(slice); i++ {
+		newInts[newItensLen] = slice[i]
+		newItensLen--
 	}
 
-	moto := model.Moto{
-		Automovel:   automovelMoto,
-		Cilindradas: 125,
-	}
+	return newInts
 
-	fmt.Println(moto.Modelo)
 }
