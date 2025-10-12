9b566daf39119fa03225c979689965042efe49ff
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-12 14:30:30 -0300

    Defer

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+func showText() {
+	fmt.Println("Finalizando a manipulacao do arquivo")
+}
 
 func main() {
-	_, err := os.Open("f:/settings.txt")
+	file, err := os.Create("./settings.txt")
+	defer file.Close()
+	defer showText()
 
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.Write([]byte("teste"))
 	if err != nil {
 		panic(err)
 	}
