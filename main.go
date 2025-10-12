039f80d7aa649d24199773439423333437183245
Patricia Souza <patricia.sz.br@gmail.com>
2025-10-12 14:19:34 -0300

    Panic

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
-import "fmt"
+import "os"
 
 func main() {
-	slice1 := []int{5, 1, 2, 3}
-	slice2 := []string{"A", "B", "C", "D"}
-	newInts := reverse((slice1))
-	newString := reverse((slice2))
-	fmt.Println(newInts)
-	fmt.Println(newString)
-}
+	_, err := os.Open("f:/settings.txt")
 
-func reverse[T int | string](slice []T) []T {
-	newInts := make([]T, len(slice))
-
-	newItensLen := len(slice) - 1
-	for i := 0; i < len(slice); i++ {
-		newInts[newItensLen] = slice[i]
-		newItensLen--
+	if err != nil {
+		panic(err)
 	}
-
-	return newInts
-
 }
